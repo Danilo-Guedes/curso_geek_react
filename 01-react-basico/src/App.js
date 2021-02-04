7ce39bbb650cfa8450a7c7cefd4a987d1fd2121a
Danilo Guedes <danilotiba@gmail.com>
2021-02-04 02:15:38 -0300

    primeiro projeto de react

---
+++ 01-react-basico/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+import Comentario from './components/Comentario'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Meu projeto</h1>
+      <Comentario />
+      <Comentario />
+      <Comentario />
+       
+    </div>
+  );
+}
+
+export default App;
---
+++ 01-react-basico/src/components/Comentario.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Comentario.css';
+
+const Comentario = () => (
+
+    //JSX
+    <div className='Comentario'>
+        <div>Mestre disse:</div>
+        <div>Muito legal este curso de React</div>
+    </div>
+
+);
+
+export default Comentario;
