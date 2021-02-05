7d8d0cdf6885c8af25b47b2c8da1d2d5bd31ffb6
Danilo Guedes <danilotiba@gmail.com>
2021-02-05 02:23:07 -0300

    aprendendo sobre component state

--- 01-react-basico/src/App.js
+++ 01-react-basico/src/App.js
@@ -1,23 +1,45 @@
+import React, { Component } from 'react';
 import './App.css';
 
 import Comentario from './components/Comentario'
 
-function App() {
-  return (
+class App extends Component {
+
+  state = {
+    comentarios: [
+      {
+        nome: "Mestre",
+        email: "mestre@mail.com",
+        data: new Date(2021, 2, 4),
+        mensagem:"Olá alguém quer TC?"
+      },
+      {
+        nome: "Maria",
+        email: "maria@mail.com",
+        data: new Date(2021, 2, 4),
+        mensagem:"Olá mestre tudo bem? de onde vc é?"
+      }
+    ]
+  }
+  
+  render(){ return (
     <div className="App">
 
       <h1>Meu projeto</h1>
-      <Comentario nome="Mestre" email="mestre@mail.com" data={new Date(2021, 2, 4)}>
-        Olá alguém quer TC?
-      </Comentario>
 
-      <Comentario nome="Maria" email="maria@mail.com" data={new Date(2021, 2, 6)}>
-        Oi Mestre, de onde você é?
-      </Comentario>
-     
-       
+      {this.state.comentarios.map((comentario, index) => (  //parenteses pq retorna direto
+        // console.log(comentario);
+
+        <Comentario 
+            key={index}
+            nome={comentario.nome} 
+            email={comentario.email} 
+            data={comentario.data}>
+            {comentario.mensagem}
+        </Comentario>       
+      ))}     
     </div>
-  );
+  )};
 }
 
 export default App;
