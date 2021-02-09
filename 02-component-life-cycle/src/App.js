a8bab3990e5c6a2c4db84ceb706d83f5049ee422
Danilo Guedes <danilotiba@gmail.com>
2021-02-08 23:44:29 -0300

    conhecendo o clico de vida dos componentes

---
+++ 02-component-life-cycle/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import MeuComponente from './components/MeuComponente'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <MeuComponente titulo="Olá mundo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
