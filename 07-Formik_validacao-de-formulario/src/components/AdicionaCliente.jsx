07353a6cea7f96e1153d432485e57af6f815ad52
Danilo Guedes <danilotiba@gmail.com>
2021-03-16 23:14:53 -0300

    fazendo as validacoes dos inputs de forma simples com os diversos metodos de handle do formik

--- 07-Formik_validacao-de-formulario/src/components/AdicionaCliente.jsx
+++ 07-Formik_validacao-de-formulario/src/components/AdicionaCliente.jsx
@@ -8,6 +8,23 @@ const AdicionaCliente = () => {
 
       <Formik
         initialValues={{ nome: "", email: "", nascimento: "" }}
+        validate={(values) => {
+          const errors = {};
+          if (!values.nome) {
+            errors.nome = "O nome é obrigatório";
+          }
+          if (!values.email) {
+            errors.email = "O email é obrigatório";
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          ) {
+            errors.email = "Email invalido";
+          }
+          if (!values.nascimento) {
+            errors.nascimento = "O nascimento é obrigatório";
+          }
+          return errors;
+        }}
         onSubmit={(values) => {
           alert(JSON.stringify(values));
         }}
@@ -20,8 +37,18 @@ const AdicionaCliente = () => {
                 id="nome"
                 name="nome"
                 type="text"
+                value={props.values.nome}
                 onChange={props.handleChange}
+                onBlur={props.handleBlur}
+                className={
+                  props.errors.nome && props.touched.nome
+                    ? "is-invalid-input"
+                    : ""
+                }
               />
+              {props.errors.nome && props.touched.nome ? (
+                <div className="invalid-input">{props.errors.nome}</div>
+              ) : null}
             </div>
             <div className="form-group">
               <label htmlFor="email">Email</label>
@@ -29,8 +56,18 @@ const AdicionaCliente = () => {
                 id="email"
                 name="email"
                 type="email"
+                value={props.values.email}
                 onChange={props.handleChange}
+                onBlur={props.handleBlur}
+                className={
+                  props.errors.email && props.touched.email
+                    ? "is-invalid-input"
+                    : ""
+                }
               />
+              {props.errors.email && props.touched.email ? (
+                <div className="invalid-input">{props.errors.email}</div>
+              ) : null}
             </div>
             <div className="form-group">
               <label htmlFor="date">Data de Nascimento</label>
@@ -38,8 +75,18 @@ const AdicionaCliente = () => {
                 id="nascimento"
                 name="nascimento"
                 type="date"
+                value={props.values.nascimento}
                 onChange={props.handleChange}
+                onBlur={props.handleBlur}
+                className={
+                  props.errors.nascimento && props.touched.nascimento
+                    ? "is-invalid-input"
+                    : ""
+                }
               />
+              {props.errors.nascimento && props.touched.nascimento ? (
+                <div className="invalid-input">{props.errors.nascimento}</div>
+              ) : null}
             </div>
             <button type="submit">Adicionar</button>
           </form>
