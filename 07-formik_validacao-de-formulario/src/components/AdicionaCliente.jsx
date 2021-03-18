eefc0f927bcea03737f3472afffd096b488d5a1b
Danilo Guedes <danilotiba@gmail.com>
2021-03-17 23:15:37 -0300

    Criando Campos de Input Customizados para reaproveitar codigo usando o hook useField do Formik

--- 07-formik_validacao-de-formulario/src/components/AdicionaCliente.jsx
+++ 07-formik_validacao-de-formulario/src/components/AdicionaCliente.jsx
@@ -1,5 +1,79 @@
 import React from "react";
-import { Formik } from "formik";
+import { Formik, Field, useField } from "formik";
+
+//Usar o Fiel no lugar do input para que os métodos de handle sejam abstraídos
+// estamos criando o Meucampo usando o hook useFiel para customizar o campo
+//e reaproveitar código
+
+const MeuCampo = (props) => {
+  const [field, meta] = useField(props);
+  return (
+    <div className="form-group">
+      <label htmlFor={props.id}>{props.label}</label>
+      <input
+        {...field}
+        {...props}
+        className={meta.error && meta.touched ? "is-invalid-input" : ""}
+      />
+      {meta.error && meta.touched ? (
+        <div className="invalid-input">{meta.error}</div>
+      ) : null}
+    </div>
+  );
+};
+
+const AdicionaCliente = () => {
+  return (
+    <>
+      <h1>Cadastro de Clientes</h1>
+
+      <Formik
+        initialValues={{ nome: "", email: "", nascimento: "" }}
+        validate={(values) => {
+          const errors = {};
+          if (!values.nome) {
+            errors.nome = "O nome é obrigatório";
+          }
+          if (!values.email) {
+            errors.email = "O email é obrigatório";
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          ) {
+            errors.email = "Email invalido";
+          }
+          if (!values.nascimento) {
+            errors.nascimento = "O nascimento é obrigatório";
+          }
+          return errors;
+        }}
+        onSubmit={(values) => {
+          alert(JSON.stringify(values));
+        }}
+      >
+        {(props) => (
+          <form noValidate onSubmit={props.handleSubmit}>
+            <MeuCampo id="nome" name="nome" type="text" label="Nome" />
+            <MeuCampo id="email" name="email" type="email" label="Email" />
+            <MeuCampo
+              id="nascimento"
+              name="nascimento"
+              type="date"
+              label="Data de Nascimento"
+            />
+            <button type="submit">Adicionar</button>
+          </form>
+        )}
+      </Formik>
+    </>
+  );
+};
+
+export default AdicionaCliente;
+
+/*
+
+COMO ERA O CODIGO ANTES DE USAR O USEFIELD DO FORMIK 
+PARA CRIAR NOSSO PROPRIO CAMPO
 
 const AdicionaCliente = () => {
   return (
@@ -97,3 +171,4 @@ const AdicionaCliente = () => {
 };
 
 export default AdicionaCliente;
+*/
